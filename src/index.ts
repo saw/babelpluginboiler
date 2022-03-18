97780104b64d61495ab3b055ad6b9a2fd52bb010
Stephen Woods <saw@stephenwoods.net>
2022-03-17 20:36:52 -0700

    boilerplate done

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import * as babel from "@babel/core";
 import {NodePath, PluginObj} from "@babel/core";
+import { MemberExpression } from "@babel/types";
+
+const thisWoo = (t: typeof babel.types):MemberExpression => {
+    return t.memberExpression(t.identifier("this"), t.identifier("woo"))
+}
 
 export default function ({types: t}: typeof babel): PluginObj {
     return {
         name: "My cool plugin",
         visitor: {
             StringLiteral(path)  {
-                path.replaceWith(t.stringLiteral('woo'))
+                if(path.node.value === '$woo') {
+                    path.replaceWith(thisWoo(t))
+                }
+                //
             }
         }
     }
